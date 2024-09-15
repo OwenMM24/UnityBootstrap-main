62ad95d21e09d82ce5284724e775fa0662c4f60b
OwenMM24 <69991084+OwenMM24@users.noreply.github.com>
2024-09-15 19:13:31 -0400

    Update Key.cs for unity mini fps project

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-    [SerializeField] GameObject lockObject;
-    [SerializeField] GameObject bouncePad;
-
+    //[SerializeField] GameObject lockObject;
+    //[SerializeField] GameObject bouncePad;
+    [SerialzieField] GameObject doorToDestory;
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             //giving the player the key
-            collision.gameObject.GetComponent<Inventory>().setKey();
+            //collision.gameObject.GetComponent<Inventory>().setKey();
 
             //unlocking the bounce pad
-            bouncePad.gameObject.GetComponent<BouncePlatform>().canBounce = true;
+            //bouncePad.gameObject.GetComponent<BouncePlatform>().canBounce = true;
 
             //destroy lock
-            Destroy(lockObject);
-
+            //Destroy(lockObject);
+            Destroy(doorToDestroy);
             //destroy the key so we dont see it in the scene anymore
             Destroy(gameObject);
         }
